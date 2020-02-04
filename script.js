85acc7c56499df416cb2bef38bd2e185c7b4b730
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-02-04 14:29:40 -0500

    style buttons

--- script.js
+++ script.js
@@ -9,21 +9,5 @@ window.onload = function () {
 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//create alphabet variables and buttons
 
-var buttons = function () {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
-//set alphabet to body
-
-for (var i = 0; i < alphabet.length; i ++) {
-    letters.id = 'alphabet';
-    list = document.createElement('li');
-    list.id = 'letter';
-    list.innerHTML = alphabet[i];
-    check();
-    myButtons.appendChild(letters);
-    letters.appendChild(list);
-        }
-    }
 }
