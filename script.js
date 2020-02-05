e8c3aebe99c5f8f3e7eec788d464a52ff8ab6371
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-02-04 19:13:49 -0500

    categories JS function

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-
-
 window.onload = function () {
 
   //create variables
@@ -15,4 +11,17 @@ window.onload = function () {
   var space;  
   var getHint;
   var word;
+
+//create categories and selection function
+
+var selectCat = () {
+    if (chosenCategories === categories[0]) {
+        categoryName.innerHTML = "Test your Spell Knowledge";
+    } else if (chosenCategories === categories[1]) {
+        categoryName.innerHTML = "Do you know your characters?";
+    } else if (chosenCategories === categories[2]) {
+        categoryName.innerHTML = "Magical Beasts!";
+    }
+}
+
 }
