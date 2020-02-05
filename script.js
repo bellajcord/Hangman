bde722a69fcbe2927775693ea0554919f3002f58
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-02-04 19:45:55 -0500

    create remaining lives function

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ window.onload = function () {
   var showCatagory = document.getElementById("scatagory");
   var getHint = document.getElementById("hint");
   var showClue = document.getElementById("clue");
-//create categories and selection function
+
+  //create categories and selection function
 
 var selectCat = () {
     if (chosenCategories === categories[0]) {
@@ -26,6 +27,18 @@ var selectCat = () {
         categoryName.innerHTML = "Do you know your characters?";
     } else if (chosenCategories === categories[2]) {
         categoryName.innerHTML = "Magical Beasts!";
+    }
+}
+// create and display remaining lives functinon using if and for loop
+comments = function() {
+    showLives.innerHTML = "Only" + lives + "left";
+    if (lives < 1) {
+        showLives.innerHTML = "You killed him. You're no wizard";
+    }
+    for (var i = 0; i < guesses.length; i++) {
+        if (counter + space === guesses.length) {
+            showLives.innerHTML = "Congratulations! You might be a wizard after all!";
+        }
     }
 }
 
