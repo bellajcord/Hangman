9aac8f4ebdf5b2dad5c45087657620e68c856be3
BellaJCo <58670074+bellajco@users.noreply.github.com>
2020-02-05 18:31:57 -0500

    added code to draw each body part of the hanging man

--- script.js
+++ script.js
@@ -140,4 +140,26 @@ window.onload = function () {
         draw (60, 5, 60, 15);
     };
 
+    // code to draw each component of the hangman
+    torso = function() {
+        draw (60, 36, 60, 70);
+    };
+
+    rightArm = function() {
+        draw (60, 46, 100, 50);
+    };
+
+    leftArm = function() {
+        draw (60, 46, 20, 50);
+    };
+
+    rightLeg = function() {
+        draw (60, 70, 100, 100);
+    };
+
+    leftLeg = function() {
+        draw (60, 70, 20, 100);
+    };
+
+    drawArray =[rightLeg, leftLeg, rightArm, leftArm, torso, head, frame4, frame3, frame2, frame1];
     
